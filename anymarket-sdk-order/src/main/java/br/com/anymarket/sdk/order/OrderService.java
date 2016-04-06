f634f88124b59676f10156012aa21ad95cf45a36
Marcos Trevisan <marcos.trevisan@db1.com.br>
2016-04-06 16:13:56 -0300

    [ANYMARKET][2019] - TRAY - PEDIDO DESENV

--- anymarket-sdk-order/src/main/java/br/com/anymarket/sdk/order/OrderService.java
+++ anymarket-sdk-order/src/main/java/br/com/anymarket/sdk/order/OrderService.java
@@ -2,6 +2,7 @@ package br.com.anymarket.sdk.order;
 
 import br.com.anymarket.sdk.SDKConstants;
 import br.com.anymarket.sdk.http.headers.IntegrationHeader;
+import br.com.anymarket.sdk.http.restdsl.RestResponse;
 import br.com.anymarket.sdk.order.dto.Order;
 import br.com.anymarket.sdk.order.filters.OrderFilter;
 import br.com.anymarket.sdk.paging.Page;
@@ -22,39 +23,43 @@ public class OrderService {
             SDKConstants.ANYMARKET_HOMOLOG_API_ENDPOINT;
     }
 
-    public Order getOrder(Long idOrder, IntegrationHeader... headers) {
+    public RestResponse getOrder(Long idOrder, IntegrationHeader... headers) {
         checkNotNull(idOrder, "Erro ao recuperar pedido: Id não informado");
         return get(apiEndPointForResource.concat("/orders/{id}"))
             .headers(headers)
             .routeParam("id", idOrder.toString())
-            .getResponse()
-            .to(Order.class);
+            .getResponse();
     }
 
-    public Page<Order> getOrders(List<OrderFilter> filters, IntegrationHeader... headers) {
+    public RestResponse getOrderByPartnerId(String partnerId, IntegrationHeader... headers) {
+        checkNotNull(partnerId, "Erro ao recuperar pedido: Id não informado");
+        return get(apiEndPointForResource.concat("/orders/{id}"))
+                .headers(headers)
+                .routeParam("id", partnerId)
+                .getResponse();
+    }
+
+
+    public RestResponse getOrders(List<OrderFilter> filters, IntegrationHeader... headers) {
         return get(apiEndPointForResource.concat("/orders"))
             .headers(headers)
             .filters(filters)
-            .getResponse()
-            .to(new TypeReference<Page<Order>>() {
-            });
+            .getResponse();
     }
 
-    public Order createOrder(Order order, IntegrationHeader... headers) {
+    public RestResponse createOrder(Order order, IntegrationHeader... headers) {
         checkNotNull(order, "Erro ao criar pedido: Dados enão encontrados.");
         return post(apiEndPointForResource.concat("/orders"))
             .headers(headers)
-            .getResponse()
-            .to(Order.class);
+            .getResponse();
     }
 
-    public Order updateOrder(Order order, IntegrationHeader... headers) {
+    public RestResponse updateOrder(Order order, IntegrationHeader... headers) {
         checkNotNull(order, "Erro ao atualizar pedido: Dados enão encontrados.");
         checkNotNull(order.getId(), "Erro ao atualizar pedido: Id não informado");
         return put(apiEndPointForResource.concat("/orders/{id}"))
             .headers(headers)
             .routeParam("id", order.getId().toString())
-            .getResponse()
-            .to(Order.class);
+            .getResponse();
     }
 }
