b2f816b03e155458a24e06078fdd64fcccad2bb3
Gyowanny Queiroz <gyowanny@gmail.com>
2016-05-16 13:53:29 -0300

    Added new enum for delivery tracking

---
+++ anymarket-sdk-order/src/main/java/br/com/anymarket/sdk/order/dto/DeliveryStatus.java
@@ -0,0 +1,21 @@
+package br.com.anymarket.sdk.order.dto;
+
+/**
+ * Created by gyowannyqueiroz on 5/16/16.
+ */
+public enum DeliveryStatus {
+    UNKNOWN("Desconhecido"),
+    IN_TRANSIT("Em trânsito"),
+    DELIVERED("Entregue"),
+    HOLD_FOR_PICKUP("Aguardando retirada");
+
+    private final String description;
+
+    private DeliveryStatus(String desc) {
+        description = desc;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
