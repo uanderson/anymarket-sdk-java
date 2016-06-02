a61f8a79834d29498ebe3bc912ac1a24c0475b12
heidi.kussakawa <heidiks@gmail.com>
2016-06-02 10:58:33 -0300

    SkuMarketPlace service method's

--- anymarket-sdk-product/src/main/java/br/com/anymarket/sdk/product/SkuMarketPlaceService.java
+++ anymarket-sdk-product/src/main/java/br/com/anymarket/sdk/product/SkuMarketPlaceService.java
@@ -1,9 +1,23 @@
 package br.com.anymarket.sdk.product;
 
 import br.com.anymarket.sdk.SDKConstants;
+import br.com.anymarket.sdk.exception.NotFoundException;
 import br.com.anymarket.sdk.http.HttpService;
+import br.com.anymarket.sdk.http.Response;
+import br.com.anymarket.sdk.http.headers.IntegrationHeader;
+import br.com.anymarket.sdk.paging.Page;
+import br.com.anymarket.sdk.product.dto.SkuMarketPlace;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.google.common.collect.Lists;
+import com.mashape.unirest.request.GetRequest;
+import com.mashape.unirest.request.body.RequestBodyEntity;
+import org.apache.http.HttpStatus;
+
+import java.util.List;
+import java.util.Objects;
 
 import static com.google.common.base.Strings.isNullOrEmpty;
+import static java.lang.String.format;
 
 public class SkuMarketPlaceService extends HttpService {
 
@@ -13,5 +27,55 @@ public class SkuMarketPlaceService extends HttpService {
     public SkuMarketPlaceService(final String apiEndPoint) {
         this.apiEndPoint = !isNullOrEmpty(apiEndPoint) ? apiEndPoint :
             SDKConstants.ANYMARKET_HOMOLOG_API_ENDPOINT;
+    }
+
+    public SkuMarketPlace insert(final SkuMarketPlace skuMp, IntegrationHeader... headers) {
+        Objects.requireNonNull(skuMp, "Informe o SkuMarketPlace a ser persistido.");
+        RequestBodyEntity post = post(getURL(), skuMp, headers);
+        Response response = execute(post);
+        return response.to(SkuMarketPlace.class);
+    }
+
+    public SkuMarketPlace update(final SkuMarketPlace skuMp,IntegrationHeader... headers) {
+        Objects.requireNonNull(skuMp, "Informe o SkuMarketPlace a ser atualizado.");
+        RequestBodyEntity put = put(getURL().concat("/").concat(skuMp.getId().toString()), skuMp, headers);
+        Response response = execute(put);
+        return response.to(SkuMarketPlace.class);
+    }
+
+    private String getURL() {
+        return String.format(apiEndPoint.concat(SKU_URI));
+    }
+
+    public SkuMarketPlace save(final SkuMarketPlace skuMp, IntegrationHeader... headers) {
+        if (skuMp.getId() != null) {
+            return update(skuMp, headers);
+        } else {
+            return insert(skuMp, headers);
+        }
+    }
+
+    public SkuMarketPlace getSkuMarketPlace(Long idSkuMp, IntegrationHeader... headers) {
+        Objects.requireNonNull(idSkuMp, "Informe o id do SkuMarketPlace.");
+        GetRequest getRequest = get(getURL().concat("/").concat(idSkuMp.toString()), headers);
+        Response response = execute(getRequest);
+        if (response.getStatus() == HttpStatus.SC_OK) {
+            return response.to(SkuMarketPlace.class);
+        }
+        throw new NotFoundException(format("Sku with id %s not found.", idSkuMp));
+    }
+
+    public List<SkuMarketPlace> getAllSkuMps(IntegrationHeader... headers) {
+        final List<SkuMarketPlace> allSkuMps = Lists.newArrayList();
+        final GetRequest getRequest = get(getURL().concat("/"), headers);
+        final Response response = execute(getRequest);
+        if (response.getStatus() == HttpStatus.SC_OK) {
+            Page<SkuMarketPlace> rootResponse = response.to(new TypeReference<Page<SkuMarketPlace>>() {
+            });
+            allSkuMps.addAll(rootResponse.getContent());
+        } else {
+            throw new NotFoundException("SkuMps not found.");
+        }
+        return allSkuMps;
     }
 }
