75676317557d8e23975cd45190902bc227c6dc00
Jose Eduardo de Almeida Junior <joeduardo_almeidajr@hotmail.com>
2016-09-20 16:20:26 -0300

    ADICIONADO STATECODE PARA CONVERTER O NOME DO ESTADO PARA SIGLA

---
+++ anymarket-sdk-common/src/main/java/br/com/anymarket/sdk/util/StateCodes.java
@@ -0,0 +1,44 @@
+package br.com.anymarket.sdk.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StateCodes {
+    private static final Map<String, String> STATES = new HashMap<String, String>() {
+        {
+            put("SAO PAULO","SP");
+            put("ACRE","AC");
+            put("ALAGOAS","AL");
+            put("AMAPA","AP");
+            put("AMAZONAS","AM");
+            put("BAHIA","BA");
+            put("CEARA","CE");
+            put("BRASILIA","DF");
+            put("DISTRITO FEDERAL","DF");
+            put("ESPIRITO SANTO","ES");
+            put("GOIAS","GO");
+            put("MARANHAO","MA");
+            put("MATO GROSSO","MT");
+            put("MATO GROSSO DO SUL","MS");
+            put("MINAS GERAIS","MG");
+            put("PARA","PA");
+            put("PARAIBA","PB");
+            put("PARANA","PR");
+            put("PERNAMBUCO","PE");
+            put("PIAUI","PI");
+            put("RIO DE JANEIRO","RJ");
+            put("RIO GRANDE DO NORTE","RN");
+            put("RIO GRANDE DO SUL","RS");
+            put("RONDONIA","RO");
+            put("RORAIMA","RR");
+            put("SANTA CATARINA","SC");
+            put("SERGIPE","SE");
+            put("TOCANTINS","TO");
+        }
+    };
+
+    public static String getStateCode(String name) {
+        return STATES.get(name);
+    }
+
+}
