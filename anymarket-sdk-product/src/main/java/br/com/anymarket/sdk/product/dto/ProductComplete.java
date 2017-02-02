7b0732e4730b29587e345a0dc39a7a181eb3c936
marcio.scharam <Mdb114>
2017-02-02 13:37:06 -0200

    support to ProductComplete and SkuComplete

--- anymarket-sdk-product/src/main/java/br/com/anymarket/sdk/product/dto/ProductComplete.java
+++ anymarket-sdk-product/src/main/java/br/com/anymarket/sdk/product/dto/ProductComplete.java
@@ -4,7 +4,10 @@ package br.com.anymarket.sdk.product.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ProductComplete extends ProductBase {
@@ -18,6 +21,43 @@ public class ProductComplete extends ProductBase {
 
     public void setSkus(List<SkuComplete> skus) {
         this.skus = skus;
+    }
+
+    public List<Image> getImagesForSku(SkuComplete sku) {
+
+        if (sku.getVariations().isEmpty()) {
+            return this.getImagesWithoutVariationValue();
+        } else {
+            return this.getImagesByVariationValues(sku.getVariations());
+        }
+    }
+
+    public List<Image> getImagesByVariationValues(List<Variation> variationValues) {
+        List<Image> images = new ArrayList<Image>();
+        for (Image image : this.getImages()) {
+            for (Variation variation : variationValues) {
+                if (hasNotVariationValue(image) || variation.getValue().equals(image.getVariation())) {
+                    images.add(image);
+                    break;
+                }
+            }
+
+        }
+        return images;
+    }
+
+    public List<Image> getImagesWithoutVariationValue() {
+        List<Image> images = new ArrayList<Image>();
+        for (Image image : this.getImages()) {
+            if (hasNotVariationValue(image)) {
+                images.add(image);
+            }
+        }
+        return images;
+    }
+
+    private boolean hasNotVariationValue(Image image) {
+        return isNullOrEmpty(image.getVariation());
     }
 
 }
