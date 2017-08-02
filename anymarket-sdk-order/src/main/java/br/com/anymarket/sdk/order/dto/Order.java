e048a5219c64aad64cbc36861b7635739c093339
marcio.scharam <Mdb114>
2017-08-02 18:04:23 -0300

    new order fields for totals

--- anymarket-sdk-order/src/main/java/br/com/anymarket/sdk/order/dto/Order.java
+++ anymarket-sdk-order/src/main/java/br/com/anymarket/sdk/order/dto/Order.java
@@ -68,6 +68,9 @@ public class Order {
     @JsonProperty("freight")
     private BigDecimal freight;
 
+    /**
+     * @deprecated Use productNet to POST and productGross/productNet to GET
+     */
     @JsonProperty("gross")
     @Deprecated
     private BigDecimal gross;
@@ -223,11 +226,17 @@ public class Order {
         this.freight = freight;
     }
 
+    /**
+     * @deprecated Use productNet to POST and productGross/productNet to GET
+     */
     @Deprecated
     public BigDecimal getGross() {
         return gross;
     }
 
+    /**
+     * @deprecated Use productNet to POST and productGross/productNet to GET
+     */
     @Deprecated
     public void setGross(BigDecimal gross) {
         this.gross = gross;
@@ -345,6 +354,22 @@ public class Order {
         this.anymarketOrderAddress = anymarketOrderAddress;
     }
 
+    public BigDecimal getProductGross() {
+        return productGross;
+    }
+
+    public void setProductGross(BigDecimal productGross) {
+        this.productGross = productGross;
+    }
+
+    public BigDecimal getProductNet() {
+        return productNet;
+    }
+
+    public void setProductNet(BigDecimal productNet) {
+        this.productNet = productNet;
+    }
+
     @Override
     public String toString() {
         return MoreObjects.toStringHelper(this)
@@ -363,7 +388,6 @@ public class Order {
             .add("marketPlaceStatusComplement", marketPlaceStatusComplement)
             .add("discount", discount)
             .add("freight", freight)
-            .add("gross", gross)
             .add("productNet", productNet)
             .add("productGross", productGross)
             .add("total", total)
