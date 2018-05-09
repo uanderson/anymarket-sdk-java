d0bff8d8d13421ff79660b55f7fe7c6731024a3a
marcos.yano <toshisan@gmail.com>
2018-05-09 11:56:45 -0300

    [ANYMARKET][6968] - [DEV] 82446 - AMAZON - UTILIZAR TEMPLATE NO ENVIO DE DESCRIÇÃO (79048 ETAPA 2)

--- anymarket-sdk-template/src/main/java/br/com/anymarket/sdk/template/TemplateService.java
+++ anymarket-sdk-template/src/main/java/br/com/anymarket/sdk/template/TemplateService.java
@@ -2,6 +2,7 @@ package br.com.anymarket.sdk.template;
 
 import br.com.anymarket.sdk.MarketPlace;
 import br.com.anymarket.sdk.SDKConstants;
+import br.com.anymarket.sdk.exception.HttpClientException;
 import br.com.anymarket.sdk.exception.NotFoundException;
 import br.com.anymarket.sdk.http.HttpService;
 import br.com.anymarket.sdk.http.Response;
@@ -22,6 +23,7 @@ import static java.lang.String.format;
 public class TemplateService extends HttpService {
 
     private static final String TEMPLATE_URI = "/template";
+    public static final String SEPARATOR = "/";
     private final String apiEndPoint;
 
     public TemplateService(String apiEndPoint) {
@@ -30,7 +32,7 @@ public class TemplateService extends HttpService {
     }
 
     public Template getTemplate(Long id, IntegrationHeader... headers) {
-        GetRequest getRequest = get(apiEndPoint + TEMPLATE_URI + id, headers);
+        GetRequest getRequest = get(apiEndPoint + TEMPLATE_URI + SEPARATOR + id, headers);
         Response response = execute(getRequest);
         if (response.getStatus() == HttpStatus.SC_OK) {
             return response.to(Template.class);
@@ -39,17 +41,29 @@ public class TemplateService extends HttpService {
     }
 
     public List<Template> getTemplatesByMarketPlace(final MarketPlace marketPlace, IntegrationHeader... headers) {
-        return getTemplateList(apiEndPoint.concat(TEMPLATE_URI).concat("/list/").concat(SDKUrlEncoder.encodeParameterToUTF8(marketPlace.name())), headers);
+        return getTemplateList(apiEndPoint.concat(TEMPLATE_URI).concat("/list/")
+            .concat(SDKUrlEncoder.encodeParameterToUTF8(marketPlace.name())),
+            headers);
     }
+
+    public String renderTemplate(Long templateId, Long skuMpId, IntegrationHeader... headers) {
+        GetRequest getRequest = get(apiEndPoint + TEMPLATE_URI + "/render/" + templateId + SEPARATOR + skuMpId, headers);
+        Response response = execute(getRequest);
+        if (response.getStatus() == HttpStatus.SC_OK) {
+            return response.getMessage();
+        }
+        throw new HttpClientException(format("Could not render template with id %d for sku id %d", templateId, skuMpId));
+    }
+
 
     private List<Template> getTemplateList(final String url, IntegrationHeader... headers) {
         final List<Template> templates = Lists.newArrayList();
         final GetRequest getRequest = get(url, headers);
         final Response response = execute(getRequest);
         if (response.getStatus() == HttpStatus.SC_OK) {
-            Page<Template> rootResponse = response.to(new TypeReference<Page<Template>>() {
+            List<Template> rootResponse = response.to(new TypeReference<List<Template>>() {
             });
-            templates.addAll(rootResponse.getContent());
+            templates.addAll(rootResponse);
         } else {
             throw new NotFoundException("Template not found.");
         }
