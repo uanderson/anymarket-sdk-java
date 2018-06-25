df3353cbbd08e133598c1b2f695893d44dfc3f41
Shaykie <joeduardo_almeidajr@hotmail.com>
2018-06-24 22:42:50 -0300

    [ANYMARKET] UPDATE PRICES SKU MARKETPLACE

---
+++ anymarket-sdk-product/src/main/java/br/com/anymarket/sdk/product/dto/SkuMarketplacePriceResource.java
@@ -0,0 +1,32 @@
+package br.com.anymarket.sdk.product.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.math.BigDecimal;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SkuMarketplacePriceResource {
+
+    @JsonProperty(value = "price")
+    private BigDecimal price;
+
+    @JsonProperty(value = "discountPrice")
+    private BigDecimal discountPrice;
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getDiscountPrice() {
+        return discountPrice;
+    }
+
+    public void setDiscountPrice(BigDecimal discountPrice) {
+        this.discountPrice = discountPrice;
+    }
+}
