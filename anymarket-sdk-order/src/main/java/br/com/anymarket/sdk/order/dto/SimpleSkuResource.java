b6ecc33aec3fea39c50854be82c7b377fd04a4b7
Jose Eduardo de Almeida Junior <joeduardo_almeidajr@hotmail.com>
2018-06-28 15:55:46 -0300

    [ANYMARKET] - SARAIVA MARKETPLACE

--- anymarket-sdk-order/src/main/java/br/com/anymarket/sdk/order/dto/SimpleSkuResource.java
+++ anymarket-sdk-order/src/main/java/br/com/anymarket/sdk/order/dto/SimpleSkuResource.java
@@ -6,8 +6,19 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SimpleSkuResource {
 
+    @JsonProperty("id")
+    private Long id;
+
     @JsonProperty("partnerId")
     private String partnerId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getPartnerId() {
         return partnerId;
