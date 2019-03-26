4fbc89f374d2c1ecd38d82b0759f3ddfacf06134
Leonardo Lopes <leonardo.lopes@db1.com.br>
2019-03-26 18:30:16 -0300

    [ANYMARKET][9205] - LIBERAÇÃO DE MARKETPLACE EFACIL

--- anymarket-sdk-common/src/main/java/br/com/anymarket/sdk/MarketPlace.java
+++ anymarket-sdk-common/src/main/java/br/com/anymarket/sdk/MarketPlace.java
@@ -31,7 +31,8 @@ public enum MarketPlace {
     COLOMBO("Lojas Colombo"),
     DIA_GROUP("Dia Group"),
     MIXTEL("Mixtel"),
-    OPTEMAIS("Optemais");
+    OPTEMAIS("Optemais"),
+    EFACIL("Éfacil");
 
     private String name;
 
