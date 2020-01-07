c4f4779b3917e0020583c3159b2d1f48987bc6b5
Marcia Toshie Tanimoto <marcia.tanimoto@db1.com.br>
2020-01-07 15:56:54 -0300

    [ANYMARKET][12820] - 51911 - 47782 REVISAR CONSUMO DE SEND PRODUCT - CARREFOUR

--- anymarket-sdk-product/src/main/java/br/com/anymarket/sdk/product/dto/ProductBase.java
+++ anymarket-sdk-product/src/main/java/br/com/anymarket/sdk/product/dto/ProductBase.java
@@ -3,6 +3,7 @@ package br.com.anymarket.sdk.product.dto;
 
 import br.com.anymarket.sdk.brand.dto.Brand;
 import br.com.anymarket.sdk.categories.dto.SimpleCategory;
+import br.com.anymarket.sdk.variation.Variation;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
@@ -13,7 +14,11 @@ import com.google.common.collect.Lists;
 import java.math.BigDecimal;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ProductBase {
@@ -281,6 +286,67 @@ public class ProductBase {
 
     public void setAllowAutomaticSkuMarketplaceCreation(Boolean allowAutomaticSkuMarketplaceCreation) {
         this.allowAutomaticSkuMarketplaceCreation = allowAutomaticSkuMarketplaceCreation;
+    }
+
+    public List<Image> getImagesForSku(SkuComplete sku) {
+
+        if (sku.getVariations().isEmpty()) {
+            return this.getImagesWithoutVariationValue();
+        } else {
+            return this.getImagesByVariationValues(sku.getVariations());
+        }
+    }
+
+    public List<Image> getImagesByVariationValues(List<Variation> variationValues) {
+        List<Image> images = new ArrayList<Image>();
+        for (Image image : this.getImages()) {
+            for (Variation variation : variationValues) {
+                if (hasNotVariationValue(image) || variation.getValue().equals(image.getVariation())) {
+                    images.add(image);
+                    break;
+                }
+            }
+        }
+
+        Collections.sort(images, getImageComparator());
+        return images;
+    }
+
+    private Comparator<Image> getImageComparator() {
+        return new Comparator<Image>() {
+            @Override
+            public int compare(Image o1, Image o2) {
+                if (o1.getVariation() != null) {
+                    if (o2.getVariation() != null) {
+                        return Integer.valueOf(o1.getIndex()).compareTo(o2.getIndex());
+                    } else {
+                        return -1;
+                    }
+                }
+                if (o1.getVariation() == null) {
+                    if (o2.getVariation() != null) {
+                        return 1;
+                    } else {
+                        return Integer.valueOf(o1.getIndex()).compareTo(o2.getIndex());
+                    }
+                }
+                return 0;
+            }
+        };
+    }
+
+    public List<Image> getImagesWithoutVariationValue() {
+        List<Image> images = new ArrayList<Image>();
+        for (Image image : this.getImages()) {
+            if (hasNotVariationValue(image)) {
+                images.add(image);
+            }
+        }
+        return images;
+    }
+
+    private boolean hasNotVariationValue(Image image) {
+        return isNullOrEmpty(image.getVariation());
     }
 
     @Override
