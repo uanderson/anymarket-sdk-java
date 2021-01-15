f472b86a070957a6ad8750125ef0d8df95d33167
julio.fradico <julio.fradico@db1.com.br>
2021-01-15 17:10:44 -0300

    Product Backlog Item 119637: [MARKETPLACE TESTE] Criação de ambiente Sandbox

--- anymarket-sdk-common/src/main/java/br/com/anymarket/sdk/MarketPlace.java
+++ anymarket-sdk-common/src/main/java/br/com/anymarket/sdk/MarketPlace.java
@@ -65,7 +65,8 @@ public enum MarketPlace {
     PASSARELA("Passarela"),
     AVENIDA("Avenida"),
     TIENDA_NARANJA("Tienda Naranja"),
-    BANCO_INTER("Banco Inter");
+    BANCO_INTER("Banco Inter"),
+    MARKETPLACE_TESTE("Marketplace Teste");
 
     private String name;
 
